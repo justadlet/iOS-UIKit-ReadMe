0a43cdae32e66ef45f3d889586dd228c6d859a6c
Adlet Zeineken <zeineken.adlet@gmail.com>
2020-12-22 20:12:28 +0600

    first init

---
+++ ReadMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadMe
+//
+//  Created by Adlet Zeineken on 22.12.2020.
+//  Copyright © 2020 justadlet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
